244a125d7852b56bab5d262fba8a60c20be1c9bd
akahige <akahige@kawamura-shigerutakashi-no-macbook-pro.local>
2009-06-05 22:29:59 +0900

    iWebKit 4.6.1に入れ替え public以下に配置 searchは絶賛途中中

--- start.rb
+++ start.rb
@@ -10,6 +10,12 @@ require 'hpricot'
 require 'open-uri'
 require 'nkf'
 
+helpers do
+  def toutf8(str)
+    NKF.nkf('-w', str) 
+  end
+end
+
 get '/' do
   url = "http://www.tvais.jp/"
   doc = Hpricot(open(url))
@@ -23,8 +29,18 @@ get '/' do
 end
 
 get '/search' do
-  redirect "/icookpad/search/#{URI.escape(params[:word])}" if params[:word]
-  recirect '/icookpad/'
+  if params[:date] && params[:time_range] && params[:tv_station]
+    '引数あったら検索結果だよ'
+    "#{params[:date]} #{params[:time_range]} #{params[:tv_station]}"
+
+    url = "http://www.tvais.jp/tvpg_search.php?tv_station=#{params[:tv_station]}&tele_day=#{params[:date]}&time_zone=#{params[:time_range]}"
+    doc = Hpricot(open(url))
+    toutf8(doc.at('title').inner_text)
+  else
+    # :dateと:time_rangeは必須だよ
+    '引数なかったら検索フォームだよ'
+    erb :search
+  end
 end
 
 Sinatra::Application.run
